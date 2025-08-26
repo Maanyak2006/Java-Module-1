eb33d30a9163e2ff5d4d7c5eee093fa2bcd16b1e
Maanyak2006 <maanya62006@gmail.com>
2025-08-26 21:43:12 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,11 @@
+class array{
+    public static void main(String[] args){
+        int n[]=new int[5];
+        n[0]=1;
+        n[1]=2;
+        n[2]=5;
+        n[3]=4;
+        n[4]=3;
+        System.out.println(n[4]);
+    }
+}
