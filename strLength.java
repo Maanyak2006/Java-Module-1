e857aab8ac534a88c559449f481b6fc0351ccd1f
Maanyak2006 <maanya62006@gmail.com>
2025-09-02 21:40:23 +0530

    Create  strLength.java

---
+++ strLength.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class strLength{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String s1=sc.nextLine();
+        System.out.println(s1.length());
+    }
+}
