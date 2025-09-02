fb602a9c7ddfad002cde897ea387ddad9de1a1e0
Maanyak2006 <maanya62006@gmail.com>
2025-09-02 21:09:09 +0530

    Create StringManipulation.java

---
+++ StringManipulation.java
@@ -0,0 +1,18 @@
+class StringManipulation{
+public static void main(String[] args){
+String a="Hello";
+String b="hello";
+System.out.println(a.toUpperCase());
+System.out.println(a.toLowerCase());
+System.out.println(a.length());
+System.out.println(a.charAt(0));
+if(a.equals(b)){
+System.out.println("Both strings are equal");
+}
+else{
+System.out.println("Strings are not equal");
+}
+}
+}
+
+
