af5122650779eb429cfe2f386e8780409f273256
Maanyak2006 <maanya62006@gmail.com>
2025-09-09 20:49:52 +0530

    Create StringReplace.java

---
+++ StringReplace.java
@@ -0,0 +1,7 @@
+public class StringReplace {
+    public static void main(String[] args) {
+        String originalString = "Hella Warld";
+        String replacedString = originalString.replace("a", "o");
+        System.out.println(replacedString);
+    }
+}
